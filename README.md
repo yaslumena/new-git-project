b41793e5c39891e6395b701bc6df14e7261fa302
ouattara asita <ouattaraasita06@gmail.com>
2024-06-08 11:28:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My first Git backed project
+
+welcome to my first Git backed dev project!
---
+++ mains.js
@@ -0,0 +1,6 @@
+// Fonction pour afficher mon Bonjour§
+function echoBonjour() {
+    console.log("Bonjour!");
+}
+// Appel a la fonction
+echoBonjour();
